ca6ac1b8c30933583d18fcaf3ece32a49560a75c
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-05 15:40:45 +0530

    Save users in database

--- src/main/java/com/simran/naukriMitra/services/UsersService.java
+++ src/main/java/com/simran/naukriMitra/services/UsersService.java
@@ -6,23 +6,39 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.simran.naukriMitra.entity.JobSeekerProfile;
+import com.simran.naukriMitra.entity.RecruiterProfile;
 import com.simran.naukriMitra.entity.Users;
+import com.simran.naukriMitra.repository.JobSeekerProfileRepository;
+import com.simran.naukriMitra.repository.RecruiterProfileRepository;
 import com.simran.naukriMitra.repository.UsersRepository;
 
 @Service
 public class UsersService {
 
 	private final UsersRepository usersRepository;
+	private final JobSeekerProfileRepository jobSeekerProfileRepository;
+	private final RecruiterProfileRepository recruiterProfileRepository;
 	
 	@Autowired
-	public UsersService(UsersRepository usersRepository) {
+	public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
 		this.usersRepository = usersRepository;
+		this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+		this.recruiterProfileRepository = recruiterProfileRepository;
 	}
 	
 	public Users addNew(Users users) {
 		users.setActive(true);
 		users.setRegistrationDate(new Date(System.currentTimeMillis()));
-		return usersRepository.save(users);	
+		Users savedUser = usersRepository.save(users);
+		int userTypeId = users.getUserTypeId().getUserTypeId();
+		if (userTypeId == 1) {
+			recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+		}else {
+			jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+		}
+		
+		return savedUser;	
 	}
 	
 	public Optional<Users> getUserByEmail(String email){
