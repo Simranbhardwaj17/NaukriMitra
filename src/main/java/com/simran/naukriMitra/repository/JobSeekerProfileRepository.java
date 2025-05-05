6c00242d077972cfa5295ba846e28bcf2c64522c
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-05 15:39:48 +0530

    Add support for skills to Job Seekers

---
+++ src/main/java/com/simran/naukriMitra/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,8 @@
+package com.simran.naukriMitra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.simran.naukriMitra.entity.JobSeekerProfile;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+
+}
---
+++ src/main/java/com/simran/naukriMitra/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.simran.naukriMitra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.simran.naukriMitra.entity.RecruiterProfile;
+
+public interface RecruiterProfileRepository extends  JpaRepository<RecruiterProfile, Integer> {
+}
