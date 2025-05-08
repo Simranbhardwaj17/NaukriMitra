084e1e8f2316e44f0716e114ae66711e400f1b60
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-08 22:50:50 +0530

    During registration encrypt user password

--- src/main/java/com/simran/naukriMitra/services/UsersService.java
+++ src/main/java/com/simran/naukriMitra/services/UsersService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.simran.naukriMitra.entity.JobSeekerProfile;
@@ -19,17 +20,20 @@ public class UsersService {
 	private final UsersRepository usersRepository;
 	private final JobSeekerProfileRepository jobSeekerProfileRepository;
 	private final RecruiterProfileRepository recruiterProfileRepository;
+	private final PasswordEncoder passwordEncoder;    //reference to password encoder to encrypt password 
 	
 	@Autowired
-	public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
+	public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository, PasswordEncoder passwordEncoder) {
 		this.usersRepository = usersRepository;
 		this.jobSeekerProfileRepository = jobSeekerProfileRepository;
 		this.recruiterProfileRepository = recruiterProfileRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 	
 	public Users addNew(Users users) {
 		users.setActive(true);
 		users.setRegistrationDate(new Date(System.currentTimeMillis()));
+		users.setPassword(passwordEncoder.encode(users.getPassword()));;
 		Users savedUser = usersRepository.save(users);
 		int userTypeId = users.getUserTypeId().getUserTypeId();
 		if (userTypeId == 1) {
