ac5dcd5c016daab622ef2eee02ab4b36d5ef9348
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-09 23:10:35 +0530

    Custom Authentication Success Handler

--- src/main/java/com/simran/naukriMitra/config/WebSecurityConfig.java
+++ src/main/java/com/simran/naukriMitra/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,10 +17,12 @@ import com.simran.naukriMitra.services.CustomUserDetailsService;
 public class WebSecurityConfig {
 	
 	private final CustomUserDetailsService customUserDetailsService;
+	private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 	
 	@Autowired
-	public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+	public WebSecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
 		this.customUserDetailsService = customUserDetailsService;
+		this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
 	}
 
 	private final String[] publicUrl = {"/",
@@ -46,6 +49,14 @@ public class WebSecurityConfig {
 			auth.requestMatchers(publicUrl).permitAll();
 			auth.anyRequest().authenticated();
 		});
+		
+		http.formLogin(form -> form.loginPage("/login").permitAll()
+				.successHandler(customAuthenticationSuccessHandler))
+		        .logout(logout -> {
+		        	logout.logoutUrl("/logout");
+			        logout.logoutSuccessUrl("/");
+				}).cors(Customizer.withDefaults())
+				.csrf(csrf -> csrf.disable());
 		
 		return http.build();
 	}
