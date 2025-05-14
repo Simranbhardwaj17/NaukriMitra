cfd52b22b754a28985c7983e1d31866961f8ccfa
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-14 22:23:07 +0530

    Recruiter profile service

---
+++ src/main/java/com/simran/naukriMitra/services/RecruiterProfileService.java
@@ -0,0 +1,25 @@
+package com.simran.naukriMitra.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.simran.naukriMitra.entity.RecruiterProfile;
+import com.simran.naukriMitra.repository.RecruiterProfileRepository;
+
+@Service
+public class RecruiterProfileService {
+
+	private final RecruiterProfileRepository recruiterProfileRepository;
+
+	@Autowired
+	public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+		this.recruiterProfileRepository = recruiterProfileRepository;
+	}
+
+	public Optional<RecruiterProfile> getOne(Integer id) {
+		return recruiterProfileRepository.findById(id);
+	}
+	
+}
