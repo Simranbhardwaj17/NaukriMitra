5f584ff78b90482ff98122245bbea7b31ffc4e94
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-16 21:41:30 +0530

    Add Support for File Upload of Recruiter Profile Image

--- src/main/java/com/simran/naukriMitra/controller/RecruiterProfileController.java
+++ src/main/java/com/simran/naukriMitra/controller/RecruiterProfileController.java
@@ -1,5 +1,6 @@
 package com.simran.naukriMitra.controller;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -8,8 +9,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.simran.naukriMitra.entity.RecruiterProfile;
 import com.simran.naukriMitra.entity.Users;
@@ -32,8 +36,8 @@ public class RecruiterProfileController {
 	public String recruiterProfile(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (!(authentication instanceof AnonymousAuthenticationToken)) {
-			String currentUsername = authentication.getName();
-			Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could not " + "found user"));
+			String currentUsername = authentication.getName();   //retrieve Username 
+			Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could " + "not found user"));  //give actual user from DB
 			Optional<RecruiterProfile> recruiterProfile = recruiterProfileService.getOne(users.getUserId());
 			
 			if (!recruiterProfile.isEmpty()) {
@@ -42,5 +46,30 @@ public class RecruiterProfileController {
 		}
 		
 		return "recruiter_profile";
+	}
+	
+	//creates a new recruiter pfp(in memory) based on form data
+	public String addNew(RecruiterProfile recruiterProfile, @RequestParam("image")MultipartFile multipartFile, Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			String currentUsername = authentication.getName();   //retrieve Username 
+			Users users = usersRepository.findByEmail(currentUsername).orElseThrow(() -> new UsernameNotFoundException("Could " + "not found user"));
+	
+			//Associate recruiter profile with existing user a/c
+			recruiterProfile.setUserId(users);
+			recruiterProfile.setUserAccountId(users.getUserId());
+		}
+		
+		model.addAttribute("profile", recruiterProfile);   //Add that pfp to model
+		
+		//Now for file upload for recruiter pfp img
+		String filename = "";
+		if (!multipartFile.getOriginalFilename().equals("")) {
+			filename = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+			recruiterProfile.setProfilePhoto(filename);  //set img name in recruiterProfile
+		}
+		RecruiterProfile savedUser = recruiterProfileService.addNew(recruiterProfile);   //Save recruiterProfile to DB
+		
+		String uploadDir = "photos/recruiter/" + savedUser.getUserAccountId();  //for upload directory
 	}
 }
