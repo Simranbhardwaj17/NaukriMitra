1090ef61b725c0ec5521e9a971e642499ef53a6a
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-05-26 22:58:12 +0530

    Interface for the repositoory

---
+++ src/main/java/com/simran/naukriMitra/repository/JobPostActivityRepository.java
@@ -0,0 +1,9 @@
+package com.simran.naukriMitra.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.simran.naukriMitra.entity.JobPostActivity;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+
+}
