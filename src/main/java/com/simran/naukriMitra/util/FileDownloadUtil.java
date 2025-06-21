cd38d4147d9879d15819a910acbd9b9f11aef98d
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-06-21 23:32:46 +0530

    FileDownloadUtil

---
+++ src/main/java/com/simran/naukriMitra/util/FileDownloadUtil.java
@@ -0,0 +1,23 @@
+package com.simran.naukriMitra.util;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileDownloadUtil {
+
+	private Path foundfile;
+
+    public Resource getFileAsResourse(String downloadDir, String fileName) throws IOException {
+
+        Path path = Paths.get(downloadDir);
+        Files.list(path).forEach(file -> {
+            if (file.getFileName().toString().startsWith(fileName)) {
+                foundfile = file;
+            }
+        });
+}
