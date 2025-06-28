2131f1c4ffabcd3f5d9671b26f0783efcfb7d7b6
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-06-28 19:44:18 +0530

    Global search done

--- src/main/java/com/simran/naukriMitra/controller/JobPostActivityController.java
+++ src/main/java/com/simran/naukriMitra/controller/JobPostActivityController.java
@@ -200,12 +200,47 @@ public class JobPostActivityController {
         model.addAttribute("job", job);
         model.addAttribute("location", location);
         
+        LocalDate searchDate = null;
+        List<JobPostActivity> jobPost = null;
+        boolean dateSearchFlag = true;
+        boolean remote = true;
+        boolean type = true;
+
+        if (days30) {
+            searchDate = LocalDate.now().minusDays(30);
+        } else if (days7) {
+            searchDate = LocalDate.now().minusDays(7);
+        } else if (today) {
+            searchDate = LocalDate.now();
+        } else {
+            dateSearchFlag = false;
+        }
+
+        if (partTime == null && fullTime == null && freelance == null) {
+            partTime = "Part-Time";
+            fullTime = "Full-Time";
+            freelance = "Freelance";
+            remote = false;
+        }
+
+        if (officeOnly == null && remoteOnly == null && partialRemote == null) {
+            officeOnly = "Office-Only";
+            remoteOnly = "Remote-Only";
+            partialRemote = "Partial-Remote";
+            type = false;
+        }
+
+        if (!dateSearchFlag && !remote && !type && !StringUtils.hasText(job) && !StringUtils.hasText(location)) {
+            jobPost = jobPostActivityService.getAll();
+        } else {
+            jobPost = jobPostActivityService.search(job, location, Arrays.asList(partTime, fullTime, freelance),
+                    Arrays.asList(remoteOnly, officeOnly, partialRemote), searchDate);
+        }
+        
         model.addAttribute("jobPost", jobPost);
         return "global-search";
     }
 	 
-
-	
 	@GetMapping("/dashboard/add")
 	public String addJobs(Model model) {  //new mthd for showing form
 		model.addAttribute("jobPostActivity", new JobPostActivity());
